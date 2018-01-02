d8fb29fd87d49e89448958fd292472383265595a
youngmight2016 <acadmin@126.com>
2018-01-01 19:32:10 -0800

    create genesis
    create genesis

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -15,6 +15,282 @@
 
 #include <boost/assign/list_of.hpp>
 
+
+#include "crypto/scrypt.h"
+//#include "util.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+
+//#include <stdlib.h>
+//#include <stdint.h>
+
+static const int SCRYPT_SCRATCHPAD_SIZE = 131072 + 63;
+
+void
+PBKDF2_SHA256(const uint8_t *passwd, size_t passwdlen, const uint8_t *salt,
+    size_t saltlen, uint64_t c, uint8_t *buf, size_t dkLen);
+
+static inline uint32_t le32dec(const void *pp)
+{
+        const uint8_t *p = (uint8_t const *)pp;
+        return ((uint32_t)(p[0]) + ((uint32_t)(p[1]) << 8) +
+            ((uint32_t)(p[2]) << 16) + ((uint32_t)(p[3]) << 24));
+}
+
+static inline void le32enc(void *pp, uint32_t x)
+{
+        uint8_t *p = (uint8_t *)pp;
+        p[0] = x & 0xff;
+        p[1] = (x >> 8) & 0xff;
+        p[2] = (x >> 16) & 0xff;
+        p[3] = (x >> 24) & 0xff;
+}
+
+
+static inline uint32_t be32dec(const void *pp)
+{
+        const uint8_t *p = (uint8_t const *)pp;
+        return ((uint32_t)(p[3]) + ((uint32_t)(p[2]) << 8) +
+            ((uint32_t)(p[1]) << 16) + ((uint32_t)(p[0]) << 24));
+}
+
+static inline void be32enc(void *pp, uint32_t x)
+{
+        uint8_t *p = (uint8_t *)pp;
+        p[3] = x & 0xff;
+        p[2] = (x >> 8) & 0xff;
+        p[1] = (x >> 16) & 0xff;
+        p[0] = (x >> 24) & 0xff;
+}
+
+typedef struct HMAC_SHA256Context {
+        SHA256_CTX ictx;
+        SHA256_CTX octx;
+} HMAC_SHA256_CTX;
+
+/* Initialize an HMAC-SHA256 operation with the given key. */
+static void
+HMAC_SHA256_Init(HMAC_SHA256_CTX *ctx, const void *_K, size_t Klen)
+{
+        unsigned char pad[64];
+        unsigned char khash[32];
+        const unsigned char *K = (const unsigned char *)_K;
+        size_t i;
+
+        /* If Klen > 64, the key is really SHA256(K). */
+        if (Klen > 64) {
+                SHA256_Init(&ctx->ictx);
+                SHA256_Update(&ctx->ictx, K, Klen);
+                SHA256_Final(khash, &ctx->ictx);
+                K = khash;
+                Klen = 32;
+        }
+
+        /* Inner SHA256 operation is SHA256(K xor [block of 0x36] || data). */
+        SHA256_Init(&ctx->ictx);
+        memset(pad, 0x36, 64);
+        for (i = 0; i < Klen; i++)
+                pad[i] ^= K[i];
+        SHA256_Update(&ctx->ictx, pad, 64);
+
+        /* Outer SHA256 operation is SHA256(K xor [block of 0x5c] || hash). */
+        SHA256_Init(&ctx->octx);
+        memset(pad, 0x5c, 64);
+        for (i = 0; i < Klen; i++)
+                pad[i] ^= K[i];
+        SHA256_Update(&ctx->octx, pad, 64);
+
+        /* Clean the stack. */
+        memset(khash, 0, 32);
+}
+
+/* Add bytes to the HMAC-SHA256 operation. */
+static void
+HMAC_SHA256_Update(HMAC_SHA256_CTX *ctx, const void *in, size_t len)
+{
+        /* Feed data to the inner SHA256 operation. */
+        SHA256_Update(&ctx->ictx, in, len);
+}
+
+/* Finish an HMAC-SHA256 operation. */
+static void
+HMAC_SHA256_Final(unsigned char digest[32], HMAC_SHA256_CTX *ctx)
+{
+        unsigned char ihash[32];
+
+        /* Finish the inner SHA256 operation. */
+        SHA256_Final(ihash, &ctx->ictx);
+
+        /* Feed the inner hash to the outer SHA256 operation. */
+        SHA256_Update(&ctx->octx, ihash, 32);
+
+        /* Finish the outer SHA256 operation. */
+        SHA256_Final(digest, &ctx->octx);
+
+        /* Clean the stack. */
+        memset(ihash, 0, 32);
+}
+
+/**
+ * PBKDF2_SHA256(passwd, passwdlen, salt, saltlen, c, buf, dkLen):
+ * Compute PBKDF2(passwd, salt, c, dkLen) using HMAC-SHA256 as the PRF, and
+ * write the output to buf.  The value dkLen must be at most 32 * (2^32 - 1).
+ */
+      ///////////////////////
+
+#define ROTL(a, b) (((a) << (b)) | ((a) >> (32 - (b))))
+
+static inline void xor_salsa8(uint32_t B[16], const uint32_t Bx[16])
+{
+        uint32_t x00,x01,x02,x03,x04,x05,x06,x07,x08,x09,x10,x11,x12,x13,x14,x15;
+        int i;
+
+        x00 = (B[ 0] ^= Bx[ 0]);
+        x01 = (B[ 1] ^= Bx[ 1]);
+        x02 = (B[ 2] ^= Bx[ 2]);
+        x03 = (B[ 3] ^= Bx[ 3]);
+        x04 = (B[ 4] ^= Bx[ 4]);
+        x05 = (B[ 5] ^= Bx[ 5]);
+        x06 = (B[ 6] ^= Bx[ 6]);
+        x07 = (B[ 7] ^= Bx[ 7]);
+        x08 = (B[ 8] ^= Bx[ 8]);
+        x09 = (B[ 9] ^= Bx[ 9]);
+        x10 = (B[10] ^= Bx[10]);
+        x11 = (B[11] ^= Bx[11]);
+        x12 = (B[12] ^= Bx[12]);
+        x13 = (B[13] ^= Bx[13]);
+        x14 = (B[14] ^= Bx[14]);
+        x15 = (B[15] ^= Bx[15]);
+        for (i = 0; i < 8; i += 2) {
+                /* Operate on columns. */
+                x04 ^= ROTL(x00 + x12,  7);  x09 ^= ROTL(x05 + x01,  7);
+                x14 ^= ROTL(x10 + x06,  7);  x03 ^= ROTL(x15 + x11,  7);
+
+                x08 ^= ROTL(x04 + x00,  9);  x13 ^= ROTL(x09 + x05,  9);
+                x02 ^= ROTL(x14 + x10,  9);  x07 ^= ROTL(x03 + x15,  9);
+
+                x12 ^= ROTL(x08 + x04, 13);  x01 ^= ROTL(x13 + x09, 13);
+                x06 ^= ROTL(x02 + x14, 13);  x11 ^= ROTL(x07 + x03, 13);
+
+                x00 ^= ROTL(x12 + x08, 18);  x05 ^= ROTL(x01 + x13, 18);
+                x10 ^= ROTL(x06 + x02, 18);  x15 ^= ROTL(x11 + x07, 18);
+
+                /* Operate on rows. */
+                x01 ^= ROTL(x00 + x03,  7);  x06 ^= ROTL(x05 + x04,  7);
+                x11 ^= ROTL(x10 + x09,  7);  x12 ^= ROTL(x15 + x14,  7);
+
+                x02 ^= ROTL(x01 + x00,  9);  x07 ^= ROTL(x06 + x05,  9);
+                x08 ^= ROTL(x11 + x10,  9);  x13 ^= ROTL(x12 + x15,  9);
+
+                x03 ^= ROTL(x02 + x01, 13);  x04 ^= ROTL(x07 + x06, 13);
+                x09 ^= ROTL(x08 + x11, 13);  x14 ^= ROTL(x13 + x12, 13);
+
+                x00 ^= ROTL(x03 + x02, 18);  x05 ^= ROTL(x04 + x07, 18);
+                x10 ^= ROTL(x09 + x08, 18);  x15 ^= ROTL(x14 + x13, 18);
+        }
+        B[ 0] += x00;
+        B[ 1] += x01;
+        B[ 2] += x02;
+        B[ 3] += x03;
+        B[ 4] += x04;
+        B[ 5] += x05;
+        B[ 6] += x06;
+        B[ 7] += x07;
+        B[ 8] += x08;
+        B[ 9] += x09;
+        B[10] += x10;
+        B[11] += x11;
+        B[12] += x12;
+        B[13] += x13;
+        B[14] += x14;
+        B[15] += x15;
+}
+
+void scrypt_1024_1_1_256_sp_generic(const char *input, char *output, char *scratchpad)
+{
+        uint8_t B[128];
+        uint32_t X[32];
+        uint32_t *V;
+        uint32_t i, j, k;
+
+        V = (uint32_t *)(((uintptr_t)(scratchpad) + 63) & ~ (uintptr_t)(63));
+
+        PBKDF2_SHA256((const uint8_t *)input, 80, (const uint8_t *)input, 80, 1, B, 128);
+
+        for (k = 0; k < 32; k++)
+                X[k] = le32dec(&B[4 * k]);
+
+        for (i = 0; i < 1024; i++) {
+                memcpy(&V[i * 32], X, 128);
+                xor_salsa8(&X[0], &X[16]);
+                xor_salsa8(&X[16], &X[0]);
+        }
+        for (i = 0; i < 1024; i++) {
+                j = 32 * (X[16] & 1023);
+                for (k = 0; k < 32; k++)
+                        X[k] ^= V[j + k];
+                xor_salsa8(&X[0], &X[16]);
+                xor_salsa8(&X[16], &X[0]);
+        }
+
+        for (k = 0; k < 32; k++)
+                le32enc(&B[4 * k], X[k]);
+
+        PBKDF2_SHA256((const uint8_t *)input, 80, B, 128, 1, (uint8_t *)output, 32);
+}
+
+#if defined(USE_SSE2)
+#if defined(_M_X64) || defined(__x86_64__) || defined(_M_AMD64) || (defined(MAC_OSX) && defined(__i386__))
+/* Always SSE2 */
+void scrypt_detect_sse2(unsigned int cpuid_edx)
+{
+    printf("scrypt: using scrypt-sse2 as built.\n");
+}
+#else
+/* Detect SSE2 */
+void (*scrypt_1024_1_1_256_sp)(const char *input, char *output, char *scratchpad);
+
+void scrypt_detect_sse2(unsigned int cpuid_edx)
+{
+    if (cpuid_edx & 1<<26)
+    {
+        scrypt_1024_1_1_256_sp = &scrypt_1024_1_1_256_sp_sse2;
+        printf("scrypt: using scrypt-sse2 as detected.\n");
+    }
+    else
+    {
+        scrypt_1024_1_1_256_sp = &scrypt_1024_1_1_256_sp_generic;
+        printf("scrypt: using scrypt-generic, SSE2 unavailable.\n");
+    }
+}
+#endif
+#endif
+
+void scrypt_1024_1_1_256(const char *input, char *output)
+{
+        char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
+#if defined(USE_SSE2)
+        // Detection would work, but in cases where we KNOW it always has SSE2,
+        // it is faster to use directly than to use a function pointer or conditional.
+#if defined(_M_X64) || defined(__x86_64__) || defined(_M_AMD64) || (defined(MAC_OSX) && defined(__i386__))
+        // Always SSE2: x86_64 or Intel MacOS X
+        scrypt_1024_1_1_256_sp_sse2(input, output, scratchpad);
+#else
+        // Detect SSE2: 32bit x86 Linux or Windows
+        scrypt_1024_1_1_256_sp(input, output, scratchpad);
+#endif
+#else
+        // Generic scrypt
+        scrypt_1024_1_1_256_sp_generic(input, output, scratchpad);
+#endif
+}
+
+
+
+
 using namespace std;
 using namespace boost::assign;
 
@@ -53,8 +329,8 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
-    (259201, uint256("1c9121bf9329a6234bfd1ea2d91515f19cd96990725265253f4b164283ade5dd"))
-    (424998, uint256("f31e381eedb0ed3ed65fcc98cc71f36012bee32e8efd017c4f9fb0620fd35f6b"))
+    (259201, uint256("1c9121bf9329a6234bfd1ea2d91515f19cd96990725265253f4b164283ade5dd"));
+/*    (424998, uint256("f31e381eedb0ed3ed65fcc98cc71f36012bee32e8efd017c4f9fb0620fd35f6b"))
     (616764, uint256("29dd0bd1c59484f290896687b4ffb6a49afa5c498caf61967c69a541f8191557")) //first block to use modifierV2
     (623933, uint256("c7aafa648a0f1450157dc93bd4d7448913a85b7448f803b4ab970d91fc2a7da7"))
     (791150, uint256("8e76f462e4e82d1bd21cb72e1ce1567d4ddda2390f26074ffd1f5d9c270e5e50"))
@@ -67,6 +343,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     (895400, uint256("7796a0274a608fac12d400198174e50beda992c1d522e52e5b95b884bc1beac6"))//block that serial# range is enforced
     (895991, uint256("d53013ed7ea5c325b9696c95e07667d6858f8ff7ee13fecfa90827bf3c9ae316"))//network split here
     (908000, uint256("202708f8c289b676fceb832a079ff6b308a28608339acbf7584de533619d014d"));
+	*/
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
     1510948627, // * UNIX timestamp of last checkpoint block
@@ -87,7 +364,7 @@ static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
     boost::assign::map_list_of(0, uint256("0x001"));
 static const Checkpoints::CCheckpointData dataRegtest = {
     &mapCheckpointsRegtest,
-    1454124731,
+    1514863570,
     0,
     100};
 
@@ -112,12 +389,12 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0x90;
+        pchMessageStart[0] = 0x99;
         pchMessageStart[1] = 0xc4;
-        pchMessageStart[2] = 0xfd;
+        pchMessageStart[2] = 0xfc;
         pchMessageStart[3] = 0xe9;
         vAlertPubKey = ParseHex("0000098d3ba6ba6e7423fa5cbd6a89e0a9a5348f88d332b44a5cb1a8b7ed2c1eaa335fc8dc4f012cb8241cc0bdafd6ca70c5f5448916e4e6f511bcd746ed57dc50");
-        nDefaultPort = 51472;
+        nDefaultPort = 50519;
         bnProofOfWorkLimit = ~uint256(0) >> 20; // PIVX starting difficulty is 1 / 2^12
         nSubsidyHalvingInterval = 210000;
         nMaxReorganizationDepth = 100;
@@ -135,7 +412,7 @@ public:
         nLastPOWBlock = 259200;
         nModifierUpdateBlock = 615800;
         nZerocoinStartHeight = 863787;
-        nZerocoinStartTime = 1508214600; // October 17, 2017 4:30:00 AM
+        nZerocoinStartTime = 1564863570; // October 17, 2017 4:30:00 AM
         nBlockEnforceSerialRange = 895400; //Enforce serial range starting this block
         nBlockRecalculateAccumulators = 908000; //Trigger a recalculation of accumulators
         nBlockFirstFraudulent = 891737; //First block that bad serials emerged
@@ -152,7 +429,7 @@ public:
          *     CTxOut(nValue=50.00000000, scriptPubKey=0xA9037BAC7050C479B121CF)
          *   vMerkleTree: e0028e
          */
-        const char* pszTimestamp = "U.S. News & World Report Jan 28 2016 With His Absence, Trump Dominates Another Debate";
+        const char* pszTimestamp = "Venus tester";
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -163,9 +440,44 @@ public:
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime = 1454124731;
+        genesis.nTime = 1514863570;
         genesis.nBits = 0x1e0ffff0;
         genesis.nNonce = 2402015;
+
+const int SCRYPT_SCRATCHPAD_SIZE = 131072 + 63;
+
+          //if (true && genesis.GetHash() != hashGenesisBlock)
+        if(false)
+        {
+            printf("Searching for genesis block...\n");
+            // This will figure out a valid hash and Nonce if you're
+            // creating a different genesis block:
+            uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
+            uint256 thash;
+            char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
+
+            while(true)
+            {
+                scrypt_1024_1_1_256_sp_generic(BEGIN(genesis.nVersion), BEGIN(thash), scratchpad);
+                if (thash <= hashTarget)
+                    break;
+                if ((genesis.nNonce & 0xFFF) == 0)
+                {
+                    printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+                }
+                ++genesis.nNonce;
+                if (genesis.nNonce == 0)
+                {
+                    printf("NONCE WRAPPED, incrementing time\n");
+                    ++genesis.nTime;
+                }
+            }
+            printf("block.nTime = %u \n", genesis.nTime);
+            printf("block.nNonce = %u \n", genesis.nNonce);
+            printf("block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+
+            }
+
 
         hashGenesisBlock = genesis.GetHash();
         assert(hashGenesisBlock == uint256("0x0000041e482b9b9691d98eefb48473405c0b8ec31b76df3797c74a78680ef818"));
@@ -252,7 +564,7 @@ public:
         nModifierUpdateBlock = 51197; //approx Mon, 17 Apr 2017 04:00:00 GMT
         nMaxMoneyOut = 43199500 * COIN;
         nZerocoinStartHeight = 201576;
-        nZerocoinStartTime = 1501776000;
+        nZerocoinStartTime = 1561776000;
         nBlockEnforceSerialRange = 1; //Enforce serial range starting this block
         nBlockRecalculateAccumulators = 9908000; //Trigger a recalculation of accumulators
         nBlockFirstFraudulent = 9891737; //First block that bad serials emerged
@@ -260,11 +572,11 @@ public:
         nBlockEnforceInvalidUTXO = 9902850; //Start enforcing the invalid UTXO's
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
-        genesis.nTime = 1454124731;
+        genesis.nTime = 1514863570;
         genesis.nNonce = 2402015;
 
         hashGenesisBlock = genesis.GetHash();
-        assert(hashGenesisBlock == uint256("0x0000041e482b9b9691d98eefb48473405c0b8ec31b76df3797c74a78680ef818"));
+//        assert(hashGenesisBlock == uint256("0x0000041e482b9b9691d98eefb48473405c0b8ec31b76df3797c74a78680ef818"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -329,13 +641,13 @@ public:
         nTargetTimespan = 24 * 60 * 60; // Pivx: 1 day
         nTargetSpacing = 1 * 60;        // Pivx: 1 minutes
         bnProofOfWorkLimit = ~uint256(0) >> 1;
-        genesis.nTime = 1454124731;
+        genesis.nTime = 1514863570;
         genesis.nBits = 0x207fffff;
         genesis.nNonce = 12345;
 
         hashGenesisBlock = genesis.GetHash();
         nDefaultPort = 51476;
-        assert(hashGenesisBlock == uint256("0x4f023a2120d9127b21bbad01724fdb79b519f593f2a85b60d3d79160ec5f29df"));
+//        assert(hashGenesisBlock == uint256("0x4f023a2120d9127b21bbad01724fdb79b519f593f2a85b60d3d79160ec5f29df"));
 
         vFixedSeeds.clear(); //! Testnet mode doesn't have any fixed seeds.
         vSeeds.clear();      //! Testnet mode doesn't have any DNS seeds.
